dff2b5fead56cf02f34a261c235bddd15c5486f0
Josh Goebel <dreamer3@gmail.com>
2012-01-10 00:48:17 -0500

    embrace rails-pulse as our official name and namespace our lib folder like every other gem I see

--- lib/pulse.rb
+++
@@ -1,3 +0,0 @@
-require File.dirname(__FILE__) + '/pulse_controller'
-require File.dirname(__FILE__) + '/pulse_helper'
-require File.dirname(__FILE__) + '/routes'
---
+++ lib/rails-pulse.rb
@@ -0,0 +1,3 @@
+require 'pulse/controller'
+require 'pulse/helper'
+require 'pulse/routes'
